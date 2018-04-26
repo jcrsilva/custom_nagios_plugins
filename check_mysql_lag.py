28d0b53febc1fc23eca01fa42f2a0677934f20dc
Jcrsilva <jcrsilva@users.noreply.github.com>
2018-04-26 15:15:49 +0100

    Flake8 fixes

--- check_mysql_lag.py
+++ check_mysql_lag.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-import argparse
-
-import pymysql
-import nagiosplugin
-
 """
 This is a simple python script to check the replica lag in a MySQL instance.
-If the instance is a Master, it just returns OK.
 
+If the instance is a Master, it just returns OK.
 (if you're the master then, logically, you're 0 seconds behind yourself)
 
 It uses the nagiosplugin framework to reduce boilerplate code and make outputting
@@ -21,13 +16,22 @@ __license__ = 'The Unlicense'
 __version__ = '1.0.0'
 
 
+import argparse
+
+import nagiosplugin
+
+import pymysql
+
+
 class CheckMySQLLag(nagiosplugin.Resource):
-    _mysql_connection = None
+    """CheckMySQLLag class."""
+
+    __mysql_connection = None
 
     @property
-    def mysql_connection(self):
-        if not self._mysql_connection:
-            self._mysql_connection = pymysql.connect(
+    def _mysql_connection(self):
+        if not self.__mysql_connection:
+            self.__mysql_connection = pymysql.connect(
                 host=self.args.hostname,
                 user=self.args.username,
                 password=self.args.password,
@@ -36,32 +40,42 @@ class CheckMySQLLag(nagiosplugin.Resource):
                 cursorclass=pymysql.cursors.DictCursor,
             )
 
-        return self._mysql_connection
+        return self.__mysql_connection
 
-    def query(self, sql):
-        with self.mysql_connection.cursor() as cur:
+    def _query(self, sql):
+        with self._mysql_connection.cursor() as cur:
             cur.execute(sql)
             return cur.fetchall()[0]
 
     def __init__(self, args):
+        """
+        Constructor.
+
+        :param args: CLI arguments.
+        """
         super(self.__class__, self).__init__()
         self.args = args
 
     def probe(self):
+        """
+        Probe method.
+
+        :return:
+        """
         sql = """
-        SELECT 
-        CASE 
+        SELECT
+        CASE
             WHEN @@global.read_only = 0 THEN true
             ELSE false
         END AS is_master;
         """
-        result = self.query(sql)
+        result = self._query(sql)
         if result['is_master'] == 1:
             return nagiosplugin.Metric('master',
                                        True,
                                        context='master')
         else:
-            result = self.query('SHOW SLAVE STATUS').get('Seconds_Behind_Master', 0)
+            result = self._query('SHOW SLAVE STATUS').get('Seconds_Behind_Master', 0)
             return nagiosplugin.Metric('lag',
                                        value=result,
                                        uom='s',
@@ -69,6 +83,11 @@ class CheckMySQLLag(nagiosplugin.Resource):
 
 
 def get_args():
+    """
+    Grab CLI arguments.
+
+    :return: CLI arguments dict.
+    """
     argp = argparse.ArgumentParser(
         description="Checks the replica lag of a MySQL Slave or returns OK if it's a Master"
     )
@@ -82,7 +101,7 @@ def get_args():
                       )
 
     argp.add_argument('-U', '--username', action='store', required=False)
-    argp.add_argument('-p', '--password', action='store', required=False, default='')
+    argp.add_argument('-p', '--password', action='store', required=False)
 
     argp.add_argument('-w', '--warning', action='store', required=True,
                       type=int,
@@ -98,12 +117,18 @@ def get_args():
 
 @nagiosplugin.guarded
 def main():
+    """
+    Main.
+
+    :return:
+    """
     args = get_args()
     nagiosplugin.Check(
         CheckMySQLLag(args),
         nagiosplugin.ScalarContext('lag', args.warning, args.critical),
         nagiosplugin.Context(name='master', fmt_metric='Is master')
     ).main()
+
 
 if __name__ == '__main__':
     main()
